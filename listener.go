133e6ca0ab83f879310048fbcbbbb3f207ff7093
Henry <cryptix@riseup.net>
2019-09-13 13:36:16 +0200

    NewListenWrapper: allow multiple ConnWrappers

--- listener.go
+++ listener.go
@@ -12,13 +12,13 @@ type ListenerWrapper func(net.Listener) (net.Listener, error)
 // NewListenerWrapper creates a new ListenerWrapper by wrapping all
 // accepted connections with the supplied connection wrapper and wrapping
 // the listener's address with the supplied address.
-func NewListenerWrapper(addr net.Addr, connWrapper ConnWrapper) ListenerWrapper {
+func NewListenerWrapper(addr net.Addr, cws ...ConnWrapper) ListenerWrapper {
 	return func(l net.Listener) (net.Listener, error) {
 		return &listener{
 			Listener: l,
 
-			addr:        WrapAddr(l.Addr(), addr),
-			connWrapper: connWrapper,
+			addr:         WrapAddr(l.Addr(), addr),
+			connWrappers: cws,
 		}, nil
 	}
 }
@@ -28,13 +28,13 @@ func NewListenerWrapper(addr net.Addr, connWrapper ConnWrapper) ListenerWrapper 
 func Listen(addr net.Addr, wrappers ...ListenerWrapper) (net.Listener, error) {
 	l, err := net.Listen(addr.Network(), addr.String())
 	if err != nil {
-		return nil, errors.Wrap(err, "error listening")
+		return nil, errors.Wrap(err, "netwrap: error listening")
 	}
 
 	for _, wrap := range wrappers {
 		l, err = wrap(l)
 		if err != nil {
-			return nil, errors.Wrap(err, "error wrapping listener")
+			return nil, errors.Wrap(err, "netwrap: error wrapping listener")
 		}
 	}
 
@@ -44,8 +44,8 @@ func Listen(addr net.Addr, wrappers ...ListenerWrapper) (net.Listener, error) {
 type listener struct {
 	net.Listener
 
-	addr        net.Addr
-	connWrapper ConnWrapper
+	addr         net.Addr
+	connWrappers []ConnWrapper
 }
 
 func (l *listener) Addr() net.Addr {
@@ -55,13 +55,16 @@ func (l *listener) Addr() net.Addr {
 func (l *listener) Accept() (net.Conn, error) {
 	conn, err := l.Listener.Accept()
 	if err != nil {
-		return nil, errors.Wrap(err, "error accepting underlying connection")
+		return nil, errors.Wrap(err, "netwrap: error accepting underlying connection")
 	}
+	origConn := conn
 
-	wrappedConn, err := l.connWrapper(conn)
-	if err != nil {
-		conn.Close()
-		return nil, errors.Wrap(err, "error in listerner wrapping function")
+	for i, cw := range l.connWrappers {
+		conn, err = cw(conn)
+		if err != nil {
+			origConn.Close()
+			return nil, errors.Wrapf(err, "netwrap: error in conn wrapping function %d", i)
+		}
 	}
-	return wrappedConn, nil
+	return conn, nil
 }
