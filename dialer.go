e0fa1a4229aa6a4664966199974be43248734d1a
Henry <cryptix@riseup.net>
2019-10-28 23:29:47 +0100

    dial: cleanup original conn if wrapping failes

--- dialer.go
+++ dialer.go
@@ -14,14 +14,16 @@ type Dialer func(net.Addr, ...ConnWrapper) (net.Conn, error)
 // Dial first opens a network connection to the supplied addr, and then applies
 // all the passed connection wrappers.
 func Dial(addr net.Addr, wrappers ...ConnWrapper) (net.Conn, error) {
-	conn, err := net.Dial(addr.Network(), addr.String())
+	origConn, err := net.Dial(addr.Network(), addr.String())
 	if err != nil {
 		return nil, errors.Wrap(err, "error dialing")
 	}
 
+	conn := origConn
 	for _, cw := range wrappers {
 		conn, err = cw(conn)
 		if err != nil {
+			origConn.Close()
 			return nil, errors.Wrap(err, "error wrapping connection")
 		}
 	}
